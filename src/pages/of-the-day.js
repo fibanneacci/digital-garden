4f0effa7e3f2bfa4050dc449ab6536964d1b86bc
‘fibanneacci’ <24998524+fibanneacci@users.noreply.github.com>
2020-12-07 12:52:21 -0600

    add random stuff

---
+++ src/pages/of-the-day.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const OfTheDayPage = () => {
+	return (
+		<Layout>
+			<SEO title="__ of the day" />
+			<p>
+				perhaps songs, things read, things learned?
+			</p>
+		</Layout>
+	);
+};
+
+export default OfTheDayPage;
