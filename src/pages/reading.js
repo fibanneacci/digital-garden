4e156dc5ec6d40a6bce34368cc25d45712ca8d3b
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-12 20:07:33 -0600

    update

---
+++ src/pages/reading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ReadingPage = () => {
+	return (
+		<Layout>
+			<SEO title="reading" />
+			<p>
+				//
+			</p>
+		</Layout>
+	);
+};
+
+export default ReadingPage;
---
+++ src/pages/watching.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const WatchingPage = () => {
+	return (
+		<Layout>
+			<SEO title="food" />
+			<p>
+				//
+			</p>
+		</Layout>
+	);
+};
+
+export default WatchingPage;
