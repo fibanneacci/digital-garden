1f351a446ef676a9d9c2bbb031acf2c87d07506a
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-01-17 11:03:34 -0600

    update

--- src/pages/discard-pile.js
+++ src/pages/discard-pile.js
@@ -8,15 +8,7 @@ const DiscardPage = () => {
             <SEO title="discard pile" />
             <h3>discard pile: words that didn't make the final cut for my blog posts</h3>
             <p>
-                Ideally, I'd like to have more control over my online existence, & not be constrained to some arbitrary and algorithmic
-                formatting decided by, say, Facebook. Then again, even things like personal websites are, to some extent, algorithmic--
-                after all, <i>search engine rankings</i> are algorithmic.
-            </p>
-            <p>
-                Recent favorites:<br />
-                Feeling: walking on a park trail with mask up and earphones in and head down, feeling very isolated from the rest of the world<br />
-                Sound: squirrels running across leaves<br />
-                Small dream: to work in an indie bookstore for a winter<br />
+                nothing here yet lol
             </p>
         </Layout>
     );
