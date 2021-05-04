dc1a785de9b6c7eec2be3ed07eb5582eba9d74d4
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-05-04 17:05:57 -0500

    update

--- src/pages/outside.js
+++ src/pages/outside.js
@@ -3,7 +3,8 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-import Firefly from '../images/firefly.png'
+import Moon from '../images/moon.png'
+import OldComputer from '../images/old_computer.png'
 
 const OutsidePage = () => {
     return (
@@ -11,19 +12,19 @@ const OutsidePage = () => {
             <SEO title="outside" />
             <div style={{ height: '30px' }} />
             <p style={{ textAlign: 'center' }}>
-                hmm, there's nothing out here...
+                hmm, there's nothing out here.
             </p>
-            <br/><br/><br/>
+            <br /><br /><br />
             <p style={{ textAlign: 'center', marginRight: '350px' }}>
                 wait...
             </p>
-            <br/><br/><br/><br/><br/><br/>
+            <br /><br /><br /><br /><br /><br />
             <p style={{ textAlign: 'center', marginLeft: '240px' }}>
                 what's this?
             </p>
-            <Link to='/outside-2' style={{ textDecoration: 'none' }}>
-                <img src={Firefly} alt='small orb of yellow light' style={{ position: 'absolute', bottom: '280px', left: '50vw', width: '50px', height: 'auto' }} />
-            </Link>
+            <a href='https://tilde.town/~al' target='_blank' rel='noreferrer' style={{ textDecoration: 'none' }}>
+                <img src={OldComputer} alt='old computer setup -- monitor, keyboard, mouse' style={{ position: 'absolute', bottom: '200px', left: '50vw', width: '70px', height: 'auto' }} />
+            </a>
         </Layout>
     );
 };
