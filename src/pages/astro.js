e360cc170ae990cdb8dfb784fab80942d883f481
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-05-09 15:20:19 -0500

    caption

--- src/pages/astro.js
+++ src/pages/astro.js
@@ -10,12 +10,12 @@ class MoonPage extends React.Component {
             mode: 'theme-dark'
         }
     }
-    
+
     componentDidMount() {
-		if (!document.documentElement.className) {
-			document.documentElement.className = this.state.mode
-		}
-		this.setState({mode: document.documentElement.className})
+        if (!document.documentElement.className) {
+            document.documentElement.className = this.state.mode
+        }
+        this.setState({ mode: document.documentElement.className })
     }
 
     render() {
@@ -25,10 +25,16 @@ class MoonPage extends React.Component {
                 <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                     <div style={{ margin: 'auto', height: '87.5vh', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                         {this.state.mode == 'theme-light' &&
-                            <iframe title='eight' src="https://open.spotify.com/embed/track/0pYacDCZuRhcrwGUA5nTBe" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                <iframe title='eight' src="https://open.spotify.com/embed/track/0pYacDCZuRhcrwGUA5nTBe" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" style={{ margin: 'auto' }}></iframe>
+                                <p><i>Under the orange sun, we’re dancing without a shadow (<a href='https://colorcodedlyrics.com/2020/05/iu-aiyu-eight-eis-prod-amp-feat-suga-of-bts' target='_blank' rel='noreferrer'>source</a>)</i></p>
+                            </div>
                         }
                         {this.state.mode == 'theme-dark' &&
-                            <iframe title='dear moon' src="https://open.spotify.com/embed/track/25jQhp7TC3nIJ3Jm3QoFI6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                <iframe title='oh, universe' allow="autoplay *; encrypted-media *; fullscreen *" frameborder="0" height="150" style={{ width: '100%', maxWidth: '660px', overflow: 'hidden', background: 'transparent', margin: 'auto' }} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/oh-universe-interlude/1109106508?i=1109106956"></iframe>
+                                <p><i>Wishing I could atomize these lonely lunar views (<a href='https://genius.com/9423322' target='_blank' rel='noreferrer'>source</a>)</i></p>
+                            </div>
                         }
                     </div>
                 </div>
