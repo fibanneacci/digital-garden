6985335b7b1f616ca486ec8ba605471f0910cecf
fibanneacci <24998524+fibanneacci@users.noreply.github.com>
2021-05-08 19:23:34 -0500

    fix focus-outline & link ordering

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,19 +50,19 @@ class IndexPage extends React.Component {
 		if (!document.documentElement.className) {
 			document.documentElement.className = this.state.mode
 		}
-		this.setState({mode: document.documentElement.className})
+		this.setState({ mode: document.documentElement.className })
 	}
 
 	toggleMode() {
 		if (this.state.mode == 'theme-light') {
-			this.setState({mode: 'theme-dark'})
+			this.setState({ mode: 'theme-dark' })
 			document.getElementById('toggle-button').textContent = 'light'
 			document.getElementById('room').src = RoomDark
 			document.getElementById('astro').src = Moon
 			document.getElementById('glow').src = Moonlight
 			document.documentElement.className = 'theme-dark'
 		} else {
-			this.setState({mode: 'theme-light'})
+			this.setState({ mode: 'theme-light' })
 			document.getElementById('toggle-button').textContent = 'dark'
 			document.getElementById('room').src = RoomLight
 			document.getElementById('astro').src = Sun
@@ -90,22 +90,25 @@ class IndexPage extends React.Component {
 						<Link to='/astro' style={{ textDecoration: 'none' }}>
 							<img id='astro' src={this.state.mode == 'theme-light' ? Sun : Moon} alt='crescent moon' style={{ position: 'absolute', top: '40px', left: '585px', width: '70px', height: 'auto' }} />
 						</Link>
-						<img src={Desk} alt='four-legged desk' style={{ position: 'absolute', top: '300px', left: '360px', width: '180px', height: 'auto' }} />
-						<img src={Chair} alt='desk chair with wheels' style={{ position: 'absolute', top: '320px', left: '405px', width: '100px', height: 'auto' }} />
-						<Link to='/words' style={{ textDecoration: 'none' }}>
-							<img src={Book} alt='open book with red cover' style={{ position: 'absolute', top: '297px', left: '450px', width: '55px', height: 'auto' }} />
-						</Link>
-						<Link to='/random' style={{ textDecoration: 'none' }}>
-							<img src={Laptop} alt='laptop' style={{ position: 'absolute', top: '280px', left: '390px', width: '60px', height: 'auto' }} />
-						</Link>
-						<a href='https://journal.anli.io' target='blank' rel='noreferrer' style={{ textDecoration: 'none '}}>
-							<img src={Clock} alt='analog clock' style={{ position: 'absolute', top: '200px', left: '340px', width: '35px', height: 'auto' }} />
-						</a>
-						<Link to='/goals' style={{ textDecoration: 'none' }}>
-							<img src={Houseplant} alt='tall leafy houseplant' style={{ position: 'absolute', top: '210px', left: '530px', width: '120px', height: 'auto' }} />
+						<Link to='/listen' style={{ textDecoration: 'none' }}>
+							<img src={Piano} alt='piano keyboard and bench' style={{ position: 'absolute', top: '350px', left: '130px', width: '175px', height: 'auto' }} />
 						</Link>
 						<Link to='/album' style={{ textDecoration: 'none' }}>
 							<img src={Photos} alt='three photos on wall' style={{ position: 'absolute', top: '200px', left: '230px', width: '50px', height: 'auto' }} />
+						</Link>
+						<a href='https://journal.anli.io' target='blank' rel='noreferrer' style={{ textDecoration: 'none ' }}>
+							<img src={Clock} alt='analog clock' style={{ position: 'absolute', top: '200px', left: '340px', width: '35px', height: 'auto' }} />
+						</a>
+						<img src={Desk} alt='four-legged desk' style={{ position: 'absolute', top: '300px', left: '360px', width: '180px', height: 'auto' }} />
+						<img src={Chair} alt='desk chair with wheels' style={{ position: 'absolute', top: '320px', left: '405px', width: '100px', height: 'auto' }} />
+						<Link to='/random' style={{ textDecoration: 'none' }}>
+							<img src={Laptop} alt='laptop' style={{ position: 'absolute', top: '280px', left: '390px', width: '60px', height: 'auto' }} />
+						</Link>
+						<Link to='/words' style={{ textDecoration: 'none' }}>
+							<img src={Book} alt='open book with red cover' style={{ position: 'absolute', top: '297px', left: '450px', width: '55px', height: 'auto' }} />
+						</Link>
+						<Link to='/goals' style={{ textDecoration: 'none' }}>
+							<img src={Houseplant} alt='tall leafy houseplant' style={{ position: 'absolute', top: '210px', left: '530px', width: '120px', height: 'auto' }} />
 						</Link>
 						<Link to='/fragments' style={{ textDecoration: 'none' }}>
 							<img src={Scroll} alt='scroll with flowers and Chinese' style={{ position: 'absolute', top: '250px', left: '650px', width: '40px', height: 'auto' }} />
@@ -116,9 +119,6 @@ class IndexPage extends React.Component {
 							<img id='open_door' src={OpenDoor} alt='brown door, open' style={{ visibility: 'hidden', position: 'absolute', top: '333px', left: '635px', width: '66px', height: 'auto' }} />
 						</Link>
 						<img id='glow' src={this.state.mode == 'theme-light' ? Sunlight : Moonlight} alt='rays of moonlight' style={{ position: 'absolute', top: '175px', left: '245px', width: '250px', height: 'auto', pointerEvents: 'none' }} />
-						<Link to='/listen' style={{ textDecoration: 'none' }}>
-							<img src={Piano} alt='piano keyboard and bench' style={{ position: 'absolute', top: '350px', left: '130px', width: '175px', height: 'auto' }} />
-						</Link>
 					</div>
 				</div>
 			</Layout>
